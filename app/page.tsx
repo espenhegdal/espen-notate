828fb3052755c9d2bb55d1555b2506f436105982
espenhegdal <espen_hegdal@hotmail.no>
2023-09-05 18:26:38 +0200

    brotha

--- app/page.tsx
+++ app/page.tsx
@@ -104,7 +104,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Hello brotha
+            Hello brothatfgdfdf df
           </p>
         </a>
       </div>
